324a8ad6dc2ef89e86b4b25a26f7546c5b8bdcb3
Corey Wofford <msucorey@gmail.com>
2016-09-21 09:06:29 -0400

    hamming
    Computes nth smallest Hamming number.  Uses heuristic assignment for
    exponent permutations to minimize CPU cycles.

---
+++ hamming.rb
@@ -0,0 +1,16 @@
+def hamming(n)
+  perm_cap = Math.sqrt(n).to_i
+  numbers = []
+  0.upto(perm_cap + 1) do |i|
+    0.upto(perm_cap) do |j|
+      0.upto(perm_cap / 2) do |k|
+        numbers << 2**i * 3**j * 5**k
+      end
+    end
+  end
+  numbers.sort[n - 1]
+end
+
+puts hamming(11)
+puts hamming(12)
+puts hamming(13)
