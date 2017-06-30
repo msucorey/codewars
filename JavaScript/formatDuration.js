57569ddf6b1abaa19d3107fa40bef811a7f4dac0
Corey Wofford <msucorey@gmail.com>
2017-06-30 14:25:05 -0400

    Solve formatDuration and get upset at lack of Oxford commas

--- JavaScript/formatDuration.js
+++ JavaScript/formatDuration.js
@@ -7,19 +7,47 @@ const HOUR_SECONDS = MINUTE_SECONDS * 60;
 const DAY_SECONDS = HOUR_SECONDS * 24;
 const YEAR_SECONDS = DAY_SECONDS * 365;
 
-function formatDuration (seconds) {
-  // Complete this function
-  if (seconds === 0) return 'now';
+function formatDuration(numSeconds) {
+  if (numSeconds === 0) return 'now';
+  const words = [];
+  let thisWord = '';
+  let remainingSeconds = numSeconds;
+  const unitsOfTime = {
+    years: Math.floor(remainingSeconds / YEAR_SECONDS),
+    days: Math.floor((remainingSeconds %= YEAR_SECONDS) / DAY_SECONDS),
+    hours: Math.floor((remainingSeconds %= DAY_SECONDS) / HOUR_SECONDS),
+    minutes: Math.floor((remainingSeconds %= HOUR_SECONDS) / MINUTE_SECONDS),
+    seconds: remainingSeconds %= MINUTE_SECONDS,
+  };
+  const wordBuilder = (timeUnitStr) => {
+    const thisUnit = unitsOfTime[timeUnitStr];
+    if (thisUnit > 0) {
+      thisWord = `${thisUnit} ${timeUnitStr}`;
+      if (thisUnit === 1) thisWord = thisWord.slice(0, -1);
+      words.push(thisWord);
+    }
+  };
+  Object.keys(unitsOfTime).forEach(timeUnit => wordBuilder(timeUnit));
+  switch (words.length) {
+    case 0:
+      return '';
+    case 1:
+      return words[0];
+    case 2:
+      return words.join(' and ');
+    default:
+      return `${words.slice(0, -1).join(', ')} and ${words[words.length - 1]}`;
+  }
 }
 
 
-formatDuration(1);
+console.log(formatDuration(1));
 // , "1 second");
-formatDuration(62);
+console.log(formatDuration(62));
 // , "1 minute and 2 seconds");
-formatDuration(120);
+console.log(formatDuration(120));
 // , "2 minutes");
-formatDuration(3600);
+console.log(formatDuration(3600));
 // , "1 hour");
-formatDuration(3662);
+console.log(formatDuration(3662));
 // , "1 hour, 1 minute and 2 seconds");
