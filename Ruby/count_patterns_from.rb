1e5987812a58ec7b5330f428e6c65a029a07412c
Corey Wofford <msucorey@gmail.com>
2018-09-15 15:27:40 -0400

    Solve screen patterns problem in Ruby

--- Ruby/count_patterns_from.rb
+++ Ruby/count_patterns_from.rb
@@ -27,17 +27,35 @@ end
 
 def other_letters(letter)
   this_and_blockers = pairs_of_blocking_letters(letter).flatten + [letter]
-  $full_grid.filter(!this_and_blockers.includes?(letter))
+  $full_grid.reject {|x| this_and_blockers.include?(x)}
 end
 
 def count_patterns_from(first_dot, length, grid = $full_grid)
   return 0 if !length.between?(1, 9)
   return 1 if length == 1
 
-  rest_of_grid = grid.filter(x != first_dot)
+  rest_of_grid = grid.reject {|x| x == first_dot}
   options = 0
 
   pairs_of_blocking_letters(first_dot).each do |pair|
-    [first_letter, blocked_letter] = pair
-    if rest_of_grid.includes?()
+    first_letter, blocked_letter = pair
+    if rest_of_grid.include?(first_letter)
+      options += count_patterns_from(first_letter, length - 1, rest_of_grid)
+    elsif rest_of_grid.include?(blocked_letter)
+      options += count_patterns_from(blocked_letter, length - 1, rest_of_grid)
+    end
+  end
+
+  other_letters(first_dot).each do |letter|
+    if rest_of_grid.include?(letter)
+      options += count_patterns_from(letter, length - 1, rest_of_grid)
+    end
+  end
+
+  options
 end
+
+puts count_patterns_from('C', 1)
+puts count_patterns_from('C', 2)
+puts count_patterns_from('C', 3)
+puts count_patterns_from('C', 4)
