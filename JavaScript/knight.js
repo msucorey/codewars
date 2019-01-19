a5e43dee0a59891c1e537f6aa695070d49c56769
Corey Wofford <msucorey@gmail.com>
2019-01-19 13:43:21 -0500

    Add additional helper functions
    possibleKnightMoves and isValidMove.

--- JavaScript/knight.js
+++ JavaScript/knight.js
@@ -1,3 +1,20 @@
+const isValidMove = move => {
+  const [x, y] = move;
+  return x >= 0 && y >= 0 && x <= 7 && y <= 7;
+}
+
+const possibleKnightMoves = startPos => {
+  const possibleMoves = [];
+  [[0, 2], [0, -2], [2, 0], [-2, 0]].forEach(firstMove => {
+    if (firstMove[0]) {
+      possibleMoves.push([firstMove[0], firstMove[1] + 1]).push([firstMove[0], firstMove[1] - 1]);
+    } else {
+      possibleMoves.push([firstMove[0] + 1, firstMove[1]]).push([firstMove[0] - 1, firstMove[1]]);
+    }
+  })
+  return possibleMoves.filter(isValidMove);
+}
+
 const coordConvert = alphaNum => {
   const [alpha, num] = alphaNum;
   const column = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].indexOf(alpha);
