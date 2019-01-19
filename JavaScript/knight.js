a3b40fed6dd2d4b2a49b98446df518ec20c5b43e
Corey Wofford <msucorey@gmail.com>
2019-01-19 15:41:28 -0500

    Fix sort bug

--- JavaScript/knight.js
+++ JavaScript/knight.js
@@ -14,12 +14,20 @@ const spacesAreAdjacent = (firstSpace, secondSpace) => {
   return Math.abs(x1 - x2) <= 1 && Math.abs(y1 - y2) <= 1;
 }
 
-const spacesAreTwoOut = (firstSpace, secondSpace) => {
+const spacesOut = (firstSpace, secondSpace, number) => {
   const [x1, y1] = firstSpace;
   const [x2, y2] = secondSpace;
   const xDiff = Math.abs(x1 - x2);
   const yDiff = Math.abs(y1 - y2);
-  return (xDiff === 2) ^ (yDiff === 2);
+  return (xDiff === number  && !yDiff) ^ (yDiff === number && !xDiff);
+}
+
+const longLeg = (firstSpace, secondSpace) => {
+  const [x1, y1] = firstSpace;
+  const [x2, y2] = secondSpace;
+  const xDiff = Math.abs(x1 - x2);
+  const yDiff = Math.abs(y1 - y2);
+  return (xDiff === 1 && yDiff == 3) || (xDiff === 3 && yDiff ==1);
 }
 
 const isValidMove = move => {
@@ -38,7 +46,7 @@ const possibleKnightMoves = startPos => {
       possibleMoves.push([firstMove[0] - 1, firstMove[1]]);
     }
   });
-  console.log('so far possible moves are ' + possibleMoves);
+  // console.log('so far possible moves are ' + possibleMoves);
   return possibleMoves.map(move => [move[0] + startPos[0], move[1] + startPos[1]]).filter(isValidMove);
 }
 
@@ -50,7 +58,7 @@ const coordConvert = coord => {
     const row = num - 1;
     return [column, row];
   }
-  console.log('coord is' + coord);
+  // console.log('coord is' + coord);
   const [x, y] = coord;
   return columnLetters[x] + (y + 1);
 }
@@ -58,21 +66,27 @@ const coordConvert = coord => {
 function knight(start, finish) {
   if (start === finish) return 0;
   const startSpace = coordConvert(start);
-  console.log('startspace is ' + startSpace);
+  // console.log('startspace is ' + startSpace);
   const finishSpace = coordConvert(finish);
-  console.log('finishspace is ' + finishSpace);
+  // console.log('finishspace is ' + finishSpace);
   if (spacesAreAdjacent(startSpace, finishSpace)) return 2;
-  if (spacesAreTwoOut(startSpace, finishSpace)) return 2;
+  if (spacesOut(startSpace, finishSpace, 2)) return 2;
+  if (longLeg(startSpace, finishSpace)) return 2;
+  if (spacesOut(startSpace, finishSpace, 5)) return 3;
   const possibleMoves = possibleKnightMoves(startSpace);
   console.log('possible moves are ' + possibleMoves);
   const distances = possibleMoves.map(space => distanceBetweenSpaces(space, finishSpace));
   console.log('distances are ' + distances);
-  const minDistance = [...distances].sort()[0];
+  const minDistance = [...distances].sort((a, b) => a - b)[0];
+  console.log('min distance is ' + minDistance);
   const minDistIndex = distances.indexOf(minDistance);
   const nextMove = possibleMoves[minDistIndex];
-  console.log('next move is ' + nextMove);
-  console.log('which would be translated to ' + coordConvert(nextMove));
+  console.log('next move is ' + coordConvert(nextMove));
   return 1 + knight(coordConvert(nextMove), finish);
 }
 
 console.log(knight('a1', 'c1'));
+console.log(knight('a1', 'f1'));
+console.log(knight('a1', 'f3'));
+console.log(knight('a1', 'f4'));
+console.log(knight('a1', 'f7'));
