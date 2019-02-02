4e07c63841575ecd19ebf6d30ebcc8d33ee6f7d6
Corey Wofford <msucorey@gmail.com>
2019-02-02 17:32:16 -0500

    Install mocha

---
+++ JavaScript/transpose.js
@@ -0,0 +1,10 @@
+function transpose(amount,tab) {
+
+  // "And if you listen very hard
+  //  The tune will come to you at last..."
+  
+  //           -- Stairway to Heaven (1971)
+  
+  }
+
+module.exports = transpose;
