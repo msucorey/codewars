7958f4e3714571e553bfadabfe73e41440d7421b
Corey Wofford <msucorey@gmail.com>
2019-02-09 14:14:04 -0500

    Implement brute force version

--- JavaScript/smallestPossibleSum/solution.js
+++ JavaScript/smallestPossibleSum/solution.js
@@ -1,5 +1,19 @@
 const solution = (numbers) => {
-  console.log(numbers);
+  let transformationMade = false;
+  let nextNumbers = numbers.sort((x, y) => x - y);
+  nextNumbers = nextNumbers.map((number, index) => {
+    if (number > nextNumbers[index - 1]) {
+      transformationMade = true;
+      return number - nextNumbers[index - 1];
+    }
+    return number;
+  });
+  console.log('next numbers is now');
+  console.log(nextNumbers);
+  if (transformationMade) {
+    return solution(nextNumbers);
+  }
+  return nextNumbers.reduce((x, y) => x + y);
 };
 
 module.exports = solution;
