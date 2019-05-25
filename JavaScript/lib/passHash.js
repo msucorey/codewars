4900eea9430157e3074b56cf07d648287dfae602
Corey Wofford <msucorey@gmail.com>
2019-05-25 11:53:08 -0400

    Implement JS solution for Codewars Password Hashes

--- JavaScript/lib/passHash.js
+++ JavaScript/lib/passHash.js
@@ -1,11 +1,30 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-function passHash(str) {
-  console.log(str);
-}
+exports.passHash = exports.default = undefined;
+
+var _crypto = require('crypto');
+
+var _crypto2 = _interopRequireDefault(_crypto);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * passHash
+ * @param {string} password - password to hash
+ *
+ * @description Hashes password using deprecated MD5 algorithm.
+ * @see https://www.codewars.com/kata/password-hashes
+ *
+ * @return {string} Lowest user rank with minimum honor/rank difference
+ */
+var passHash = function passHash(password) {
+  var hash = _crypto2.default.createHash('md5');
+  hash.update(password);
+  return hash.digest('hex');
+};
 
 exports.default = passHash;
 exports.passHash = passHash;
--- JavaScript/passHash.js
+++ JavaScript/passHash.js
@@ -1,5 +1,18 @@
-function passHash(str) {
-  console.log(str);
-}
+import crypto from 'crypto';
+
+/**
+ * passHash
+ * @param {string} password - password to hash
+ *
+ * @description Hashes password using deprecated MD5 algorithm.
+ * @see https://www.codewars.com/kata/password-hashes
+ *
+ * @return {string} Lowest user rank with minimum honor/rank difference
+ */
+const passHash = (password) => {
+  const hash = crypto.createHash('md5');
+  hash.update(password);
+  return hash.digest('hex');
+};
 
 export { passHash as default, passHash };
