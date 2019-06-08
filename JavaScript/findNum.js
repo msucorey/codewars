225ee0a8ac2cd9cde6df248c42a44ccb68603c21
Corey Wofford <msucorey@gmail.com>
2019-06-08 14:30:19 -0400

    Set up base case

--- JavaScript/findNum.js
+++ JavaScript/findNum.js
@@ -1,6 +1,10 @@
-const findNum = (n) => {
-  console.log(n);
-  return 0;
+// https://www.codewars.com/kata/unique-digit-sequence/train/
+
+const defaultInputArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const findNum = (n, array = defaultInputArray) => {
+  if (array[n]) return array[n];
+  return null;
 };
 
 export default findNum;
--- JavaScript/lib/findNum.js
+++ JavaScript/lib/findNum.js
@@ -3,9 +3,15 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+// https://www.codewars.com/kata/unique-digit-sequence/train/
+
+var defaultInputArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 var findNum = function findNum(n) {
-  console.log(n);
-  return 0;
+  var array = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : defaultInputArray;
+
+  if (array[n]) return array[n];
+  return null;
 };
 
 exports.default = findNum;
