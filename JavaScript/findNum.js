be0d2f15eecdb73ab26ac4423507d3a4cde7149c
Corey Wofford <msucorey@gmail.com>
2019-06-15 11:40:07 -0400

    Remove unnecessary spreads.

--- JavaScript/findNum.js
+++ JavaScript/findNum.js
@@ -2,13 +2,13 @@
 
 const defaultInputArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const intersection = (a, b) => [...a].filter(el => b.includes(el));
+const intersection = (a, b) => a.filter(el => b.includes(el));
 
 const nextNum = (array) => {
   const lastNum = array[array.length - 1];
   const lastNumDigits = lastNum.toString().split('');
 
-  let nextSeqNum = defaultInputArray.length;
+  let nextSeqNum = 4 + Math.floor(((defaultInputArray.length) - 10) / 2);
 
   while (true) {
     if (!array.includes(nextSeqNum)) {
@@ -21,6 +21,4 @@ const nextNum = (array) => {
 };
 
 const findNum = (n, array = defaultInputArray) =>
-  array[n] || findNum(n, [...array].concat(nextNum([...array])));
-
-export default findNum;
+  array[n] || findNum(n, array.concat(nextNum(array)));
