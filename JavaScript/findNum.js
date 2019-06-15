d4554525af98dece7e0960d18e32f7832dd5bf5f
Corey Wofford <msucorey@gmail.com>
2019-06-15 13:13:20 -0400

    Memoize intersection results

--- JavaScript/findNum.js
+++ JavaScript/findNum.js
@@ -2,23 +2,45 @@
 
 const defaultInputArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const intersection = (a, b) => a.filter(el => b.includes(el));
+let lowestNextSeqNum = 11;
+
+const noIntersectionMemo = {};
+
+const noIntersection = (num1, num2) => {
+  const num1String = num1.toString();
+  const num2String = num2.toString();
+  const combinedString = num1String + num2String;
+  if (noIntersectionMemo[combinedString]) return noIntersectionMemo[combinedString][0];
+  const num1Array = num1String.split('');
+  const num2Array = num2String.toString().split('');
+  for (let i = 0; i < num1Array.length; i += 1) {
+    if (num2Array.includes(num1Array[i])) {
+      noIntersection[combinedString] = [false];
+      return false;
+    }
+  }
+  noIntersection[combinedString] = [true];
+  return true;
+};
 
 const nextNum = (array) => {
   const lastNum = array[array.length - 1];
-  const lastNumDigits = lastNum.toString().split('');
 
-  let nextSeqNum = 4 + Math.floor(((defaultInputArray.length) - 10) / 2);
+  let nextSeqNum = lowestNextSeqNum;
 
-  while (true) {
+  let found = false;
+  while (!found) {
     if (!array.includes(nextSeqNum)) {
-      const nextSeqNumDigits = nextSeqNum.toString().split('');
-      const commonDigits = intersection(lastNumDigits, nextSeqNumDigits);
-      if (!commonDigits.length) return nextSeqNum;
+      if (noIntersection(lastNum, nextSeqNum)) found = true;
     }
-    nextSeqNum += 1;
+    if (!found) nextSeqNum += 1;
   }
+  console.log('nextSeqNum', nextSeqNum, array.length - 1);
+  if (!found && nextSeqNum === lowestNextSeqNum + 1) lowestNextSeqNum = nextSeqNum;
+  if (found) return nextSeqNum;
 };
 
 const findNum = (n, array = defaultInputArray) =>
   array[n] || findNum(n, array.concat(nextNum(array)));
+
+export default findNum;
