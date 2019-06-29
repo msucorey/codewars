f54217bed245f95ce7f059f51faa886dc236c51e
Corey Wofford <msucorey@gmail.com>
2019-06-29 09:55:27 -0400

    Add base case

--- JavaScript/connectedValues.js
+++ JavaScript/connectedValues.js
@@ -1,8 +1,11 @@
 // https://www.codewars.com/kata/connecting-values
 
 function connectedValues(arr, val, coord) {
+  const [row, column] = coord;
+  if (arr[row][column] !== val) return [];
   /* Implement */
   console.log(arr, val, coord);
+  return null;
 }
 
 export default connectedValues;
--- JavaScript/lib/test.js
+++ JavaScript/lib/test.js
@@ -4,26 +4,23 @@ var _assert = require('assert');
 
 var _assert2 = _interopRequireDefault(_assert);
 
-var _findNum = require('./findNum');
+var _connectedValues = require('./connectedValues');
 
-var _findNum2 = _interopRequireDefault(_findNum);
+var _connectedValues2 = _interopRequireDefault(_connectedValues);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-describe('Basic test cases', function () {
-  it('Should compute correct value for 1', function () {
-    (0, _assert2.default)((0, _findNum2.default)(1), 1);
+describe('connectedValues()', function () {
+  var arr1 = [[0, 0, 0, 1, 3, 4, 0, 3], [0, 2, 0, 0, 2, 0, 0, 5], [0, 0, 0, 2, 0, 1, 1, 1], [2, 3, 4, 1, 3, 1, 0, 0], [0, 1, 5, 1, 6, 0, 2, 0], [2, 0, 2, 3, 1, 1, 1, 1], [2, 0, 2, 3, 1, 1, 1, 1]];
+
+  it('Should return [] when arr at coord != val', function () {
+    (0, _assert2.default)((0, _connectedValues2.default)(arr1, 0, [4, 2]), []);
   });
-  it('Should compute correct value for 5', function () {
-    (0, _assert2.default)((0, _findNum2.default)(5), 5);
+  it('Should return the correct array of coordinates with length == 1', function () {
+    (0, _assert2.default)((0, _connectedValues2.default)(arr1, 2, [1, 1]), [[1, 1]]);
   });
-  it('Should compute correct value for 22', function () {
-    (0, _assert2.default)((0, _findNum2.default)(11), 22);
+  it('Should return the correct array of coordinates with length > 1', function () {
+    var ans1 = [[0, 0], [0, 1], [0, 2], [0, 6], [1, 0], [1, 2], [1, 3], [1, 5], [1, 6], [2, 0], [2, 1], [2, 2], [2, 4]];
+    (0, _assert2.default)((0, _connectedValues2.default)(arr1, 0, [0, 0]).sort(), ans1);
   });
-  it('Should compute correct value for 103', function () {
-    (0, _assert2.default)((0, _findNum2.default)(100), 103);
-  });
-  // it('Should compute correct value for 476', () => {
-  //   assert(findNum(500), 476);
-  // });
 });
