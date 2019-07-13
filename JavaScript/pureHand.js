b100eb2e7f241ad62f391f92363abe8d767f589c
Corey Wofford <msucorey@gmail.com>
2019-07-13 11:12:36 -0400

    Set up TDD for pureHand

---
+++ JavaScript/pureHand.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/mahjong-number-1-pure-hand
+
+const solution = (tiles) => {
+  console.log(tiles);
+  return '';
+}
+
+const pureHand = solution;
+
+export default pureHand;
--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,27 +1,17 @@
 import assert from 'assert';
 
-import connectedValues from './connectedValues';
+import pureHand from './pureHand';
 
-describe('connectedValues()', () => {
-  const arr1 = [
-    [0, 0, 0, 1, 3, 4, 0, 3],
-    [0, 2, 0, 0, 2, 0, 0, 5],
-    [0, 0, 0, 2, 0, 1, 1, 1],
-    [2, 3, 4, 1, 3, 1, 0, 0],
-    [0, 1, 5, 1, 6, 0, 2, 0],
-    [2, 0, 2, 3, 1, 1, 1, 1],
-    [2, 0, 2, 3, 1, 1, 1, 1]];
+describe('pureHand()', () => {
+  const cases = [
+    ['1113335557779', '89'],
+    ['1223334455678', '258'],
+  ];
 
-  it('Should return [] when arr at coord != val', () => {
-    assert(connectedValues(arr1, 0, [4, 2]), []);
-  });
-  it('Should return the correct array of coordinates with length == 1', () => {
-    assert(connectedValues(arr1, 2, [1, 1]), [[1, 1]]);
-  });
-  it('Should return the correct array of coordinates with length > 1', () => {
-    const ans1 = [[0, 0], [0, 1], [0, 2], [0, 6], [1, 0],
-      [1, 2], [1, 3], [1, 5], [1, 6], [2, 0],
-      [2, 1], [2, 2], [2, 4]];
-    assert(connectedValues(arr1, 0, [0, 0]).sort(), ans1);
-  });
+  // eslint-disable-next-line no-restricted-syntax
+  for (const [hand, expected] of cases) {
+    it('hand should be expected', () => {
+      assert(pureHand(hand), expected);
+    });
+  }
 });
