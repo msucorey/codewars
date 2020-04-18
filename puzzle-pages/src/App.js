30d470861518e6faf5b082513668df89a5a38910
Corey Wofford <msucorey@gmail.com>
2020-04-18 13:59:02 -0400

    Remove link and service worker

--- puzzle-pages/src/App.js
+++ puzzle-pages/src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="app__header">
+        <img src={logo} className="app__logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Puzzle Pages</p>
       </header>
     </div>
   );
