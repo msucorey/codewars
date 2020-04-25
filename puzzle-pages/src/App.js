3fb9e72cb7856d360630539cb3e8b5941caa61e2
Corey Wofford <msucorey@gmail.com>
2020-04-25 14:11:34 -0400

    Create h1 for landing page

--- puzzle-pages/src/App.js
+++ puzzle-pages/src/App.js
@@ -2,18 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () => (
     <div className="app">
       <header className="app__header">
         <img src={logo} className="app__logo" alt="logo" />
+        <h1>Puzzle Pages</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Puzzle Pages</p>
       </header>
     </div>
   );
-}
 
 export default App;
