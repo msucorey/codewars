ceaa7f76b520826c6c2af137ac06690c5810a3e5
Corey Wofford <msucorey@gmail.com>
2020-05-02 14:24:51 -0400

    Add link to About page

--- puzzle-pages/src/pages/Home/Home.js
+++ puzzle-pages/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 // TODO install Typescript
 
@@ -11,6 +12,9 @@ const Home = () => (
         <h1>Puzzle Pages</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>{`Why don't you check out our `}
+          <Link to="/about" className="home__header__link">about</Link>{` page`}.
         </p>
       </header>
     </div>
