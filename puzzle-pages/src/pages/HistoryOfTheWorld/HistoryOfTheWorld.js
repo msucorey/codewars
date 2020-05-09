9cc51c26324bb0834a2497c7815be96d4774ad12
Corey Wofford <b@bob.com>
2020-05-09 10:55:10 -0400

    Correct for deck count

--- puzzle-pages/src/pages/HistoryOfTheWorld/HistoryOfTheWorld.js
+++ puzzle-pages/src/pages/HistoryOfTheWorld/HistoryOfTheWorld.js
@@ -3,44 +3,46 @@ import { factorial } from '../../util/util';
 
 // TODO write test
 
-const numCards = 71;
+const numCards = 49;
 const numDisasterCards = 4;
 const numNonDisasterCards = numCards - numDisasterCards;
-const numCardsInHand = 10;
+const numCardsInHand = 7;
 const numPlayers = 6;
 
-const singlePermOddsOf1DC = factorial(numNonDisasterCards) / factorial(numNonDisasterCards - 9) * factorial(numDisasterCards) / factorial (numDisasterCards - 1) * factorial(numCards - 10) / factorial(numCards);
-const singlePermOddsOf2DC = factorial(numNonDisasterCards) / factorial(numNonDisasterCards - 8) * factorial(numDisasterCards) / factorial (numDisasterCards - 2) * factorial(numCards - 10) / factorial(numCards);
-const singlePermOddsOf3DC = factorial(numNonDisasterCards) / factorial(numNonDisasterCards - 7) * factorial(numDisasterCards) / factorial (numDisasterCards - 3) * factorial(numCards - 10) / factorial(numCards);
-const singlePermOddsOf4DC = factorial(numNonDisasterCards) / factorial(numNonDisasterCards - 6) * factorial(numDisasterCards) * factorial(numCards - 10) / factorial(numCards);
+const singlePermOddsOf1DC = factorial(numNonDisasterCards) / factorial(numNonDisasterCards + 1 - numCardsInHand) * factorial(numDisasterCards) / factorial (numDisasterCards - 1) * factorial(numCards - numCardsInHand) / factorial(numCards);
+const singlePermOddsOf2DC = factorial(numNonDisasterCards) / factorial(numNonDisasterCards + 2 - numCardsInHand) * factorial(numDisasterCards) / factorial (numDisasterCards - 2) * factorial(numCards - numCardsInHand) / factorial(numCards);
+const singlePermOddsOf3DC = factorial(numNonDisasterCards) / factorial(numNonDisasterCards + 3 - numCardsInHand) * factorial(numDisasterCards) / factorial (numDisasterCards - 3) * factorial(numCards - numCardsInHand) / factorial(numCards);
+const singlePermOddsOf4DC = factorial(numNonDisasterCards) / factorial(numNonDisasterCards + 4 - numCardsInHand) * factorial(numDisasterCards) * factorial(numCards - numCardsInHand) / factorial(numCards);
 
-const numCombs91Mix = factorial(numCardsInHand) / factorial(numCardsInHand - 1) / factorial(1);
-const numCombs82Mix = factorial(numCardsInHand) / factorial(numCardsInHand - 2) / factorial(2);
-const numCombs73Mix = factorial(numCardsInHand) / factorial(numCardsInHand - 3) / factorial(3);
-const numCombs64Mix = factorial(numCardsInHand) / factorial(numCardsInHand - 4) / factorial(4);
+const numCombs61Mix = factorial(numCardsInHand) / factorial(numCardsInHand - 1) / factorial(1);
+const numCombs52Mix = factorial(numCardsInHand) / factorial(numCardsInHand - 2) / factorial(2);
+const numCombs43Mix = factorial(numCardsInHand) / factorial(numCardsInHand - 3) / factorial(3);
+const numCombs34Mix = numCombs43Mix;
 
-const overallOddsOf1DC = singlePermOddsOf1DC * numCombs91Mix;
-const overallOddsOf2DC = singlePermOddsOf2DC * numCombs82Mix;
-const overallOddsOf3DC = singlePermOddsOf3DC * numCombs73Mix;
-const overallOddsOf4DC = singlePermOddsOf4DC * numCombs64Mix;
+const oddsOf1DC = singlePermOddsOf1DC * numCombs61Mix;
+const oddsOf2DC = singlePermOddsOf2DC * numCombs52Mix;
+const oddsOf3DC = singlePermOddsOf3DC * numCombs43Mix;
+const oddsOf4DC = singlePermOddsOf4DC * numCombs34Mix;
 
-const overallOddsOf0DC = 1 - overallOddsOf1DC - overallOddsOf2DC - overallOddsOf3DC - overallOddsOf4DC;
+const oddsOf0DC = 1 - oddsOf1DC - oddsOf2DC - oddsOf3DC - oddsOf4DC;
 
-const anyPlayerOddsOf1DC = 1 - (1 - overallOddsOf1DC) ** numPlayers;
-const anyPlayerOddsOf2DC = 1 - (1 - overallOddsOf2DC) ** numPlayers;
-const anyPlayerOddsOf3DC = 1 - (1 - overallOddsOf3DC) ** numPlayers;
-const anyPlayerOddsOf4DC = 1 - (1 - overallOddsOf4DC) ** numPlayers;
+const anyPlayerOddsOf0DC = 1 - (1 - oddsOf0DC) ** numPlayers;
+const anyPlayerOddsOf1DC = 1 - (1 - oddsOf1DC) ** numPlayers;
+const anyPlayerOddsOf2DC = 1 - (1 - oddsOf2DC) ** numPlayers;
+const anyPlayerOddsOf3DC = 1 - (1 - oddsOf3DC) ** numPlayers;
+const anyPlayerOddsOf4DC = 1 - (1 - oddsOf4DC) ** numPlayers;
 
-console.log('overallOddsOf0DC', overallOddsOf0DC);
-console.log('overallOddsOf1DC', overallOddsOf1DC);
-console.log('overallOddsOf2DC', overallOddsOf2DC);
-console.log('overallOddsOf3DC', overallOddsOf3DC);
-console.log('overallOddsOf4DC', overallOddsOf4DC);
+console.log('oddsOf0DC', oddsOf0DC);
+console.log('oddsOf1DC', oddsOf1DC);
+console.log('oddsOf2DC', oddsOf2DC);
+console.log('oddsOf3DC', oddsOf3DC);
+console.log('oddsOf4DC', oddsOf4DC);
+console.log('anyPlayerOddsOf0DC', anyPlayerOddsOf0DC);
 console.log('anyPlayerOddsOf1DC', anyPlayerOddsOf1DC);
 console.log('anyPlayerOddsOf2DC', anyPlayerOddsOf2DC);
 console.log('anyPlayerOddsOf3DC', anyPlayerOddsOf3DC);
 console.log('anyPlayerOddsOf4DC', anyPlayerOddsOf4DC);
 
-const HistoryOfTheWorld = () => <div className="about">{overallOddsOf3DC}</div>;
+const HistoryOfTheWorld = () => <div className="about">{oddsOf3DC}</div>;
 
 export default HistoryOfTheWorld;
