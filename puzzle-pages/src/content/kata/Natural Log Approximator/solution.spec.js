343287092985d47eb4ef2237aa332f71668065e7
Corey Wofford <msucorey@gmail.com>
2020-11-28 09:28:06 -0500

    Clean up and comment

--- puzzle-pages/src/content/kata/Natural Log Approximator/solution.spec.js
+++ puzzle-pages/src/content/kata/Natural Log Approximator/solution.spec.js
@@ -1,4 +1,4 @@
-import { ln, withinRange } from './solution';
+import { ln, withinRange, MIN_ACCURACY } from './solution';
 import assert from 'assert';
 
 describe("for inputs 0 and below", function() {
@@ -9,8 +9,8 @@ describe("for inputs 0 and below", function() {
 });
 describe("Basic tests", function() {
   it('should do that thing it does when it does that thing', () => {
-    assert.strictEqual(withinRange(ln(1), 0, 1E-10), true, "The natural log of 1 is 0.");
-    assert.strictEqual(withinRange(ln(2.71828), 0.9999993273, 1E-10), true, "The natural log of 2.71828 is 0.99999932...");
-    assert.strictEqual(withinRange(ln(10), 2.3025850929, 1E-10), true, "The natural log of 10 is 2.30258509...");
+    assert.strictEqual(withinRange(ln(1), 0, MIN_ACCURACY), true, "The natural log of 1 is 0.");
+    assert.strictEqual(withinRange(ln(2.71828), 0.9999993273, MIN_ACCURACY), true, "The natural log of 2.71828 is 0.99999932...");
+    assert.strictEqual(withinRange(ln(10), 2.3025850929, MIN_ACCURACY), true, "The natural log of 10 is 2.30258509...");
   });
 });
--- puzzle-pages/src/content/kata/Natural Log Approximator/solution.ts
+++ puzzle-pages/src/content/kata/Natural Log Approximator/solution.ts
@@ -2,25 +2,29 @@
 
 // @see https://www.codewars.com/kata/59a5a436589d2a3e59000036
 
+const MIN_ACCURACY = 1E-10;
+
 const withinRange = (number, target, range) => Math.abs(number - target) <= range;
 
-function ln(x: any) {
+const ln = (x: any) => {
+  // base cases
   if (x <= 0) return NaN;
   if (x === 1) return 0;
 
+  // Euler's number
   const e = 2.71828182845904523536028747135266249775724709369995;
 
-  let lowerLimit = 1E-10;
-  let upperLimit = 50;
+  let lowerLimit = MIN_ACCURACY;
+  let upperLimit = 50; // roughly ln(1E20)
 
-  while (upperLimit - lowerLimit > 1E-10) {
+  while (upperLimit - lowerLimit > MIN_ACCURACY) {
     const newLimit = (upperLimit + lowerLimit) / 2;
 
     if (e ** newLimit > x) upperLimit = newLimit;
     else lowerLimit = newLimit;
   };
 
-  return (upperLimit + lowerLimit) /2;
+  return (upperLimit + lowerLimit) / 2;
 }
 
-export { ln, withinRange };
+export { ln, withinRange, MIN_ACCURACY };
