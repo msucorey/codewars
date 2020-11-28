e05aad39e955d4dbe11fc8451ce1032e7a8c9c93
Corey Wofford <msucorey@gmail.com>
2020-11-28 09:01:35 -0500

    Relocate and refactor helper function

--- puzzle-pages/src/content/kata/Natural Log Approximator/solution.spec.js
+++ puzzle-pages/src/content/kata/Natural Log Approximator/solution.spec.js
@@ -1,19 +1,16 @@
-import { ln } from './solution';
+import { ln, withinRange } from './solution';
 import assert from 'assert';
 
-function withinRange(number, target, range) {
-  if (Math.abs(number - target) <= range) {
-    return true;
-  }
-  return false;
-}
-
-describe("Should return NaN for inputs 0 and below", function() {
-  assert.strictEqual(isNaN(ln(-10)), true, "The natural log of -10 is NaN.");
-  assert.strictEqual(isNaN(ln(0)), true, "The natural log of 0 is NaN.");
+describe("for inputs 0 and below", function() {
+  it('should return NaN', () => {
+    assert.strictEqual(isNaN(ln(-10)), true, "The natural log of -10 is NaN.");
+    assert.strictEqual(isNaN(ln(0)), true, "The natural log of 0 is NaN.");
+  });
 });
 describe("Basic tests", function() {
-  assert.strictEqual(withinRange(ln(1), 0, 1E-10), true, "The natural log of 1 is 0.");
-  assert.strictEqual(withinRange(ln(2.71828), 0.9999993273, 1E-10), true, "The natural log of 2.71828 is 0.99999932...");
-  assert.strictEqual(withinRange(ln(10), 2.3025850929, 1E-10), true, "The natural log of 10 is 2.30258509...");
+  it('should do that thing it does when it does that thing', () => {
+    assert.strictEqual(withinRange(ln(1), 0, 1E-10), true, "The natural log of 1 is 0.");
+    assert.strictEqual(withinRange(ln(2.71828), 0.9999993273, 1E-10), true, "The natural log of 2.71828 is 0.99999932...");
+    assert.strictEqual(withinRange(ln(10), 2.3025850929, 1E-10), true, "The natural log of 10 is 2.30258509...");
+  });
 });
--- puzzle-pages/src/content/kata/Natural Log Approximator/solution.ts
+++ puzzle-pages/src/content/kata/Natural Log Approximator/solution.ts
@@ -2,8 +2,15 @@
 
 // @see https://www.codewars.com/kata/59a5a436589d2a3e59000036
 
+const withinRange = (number, target, range) => Math.abs(number - target) <= range;
+
 function ln(x: any) {
-  // Return the natural log of x
+  if (x <= 0) return NaN;
+  if (x === 1) return 0;
+
+  const e = 2.71828182845904523536028747135266249775724709369995;
+
+  return x;
 }
 
-export { ln };
+export { ln, withinRange };
