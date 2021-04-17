37b4255c32e8f8a3f3d86946f119defc17939775
Corey Wofford <msucorey@gmail.com>
2021-04-17 14:31:35 -0400

    Solve All Balanced Parentheses

--- puzzle-pages/src/content/kata/All Balanced Parentheses/solution.ts
+++ puzzle-pages/src/content/kata/All Balanced Parentheses/solution.ts
@@ -10,7 +10,7 @@ balancedParens(3) => ["()()()","(())()","()(())","(()())","((()))"]
 @see https://www.codewars.com/kata/5426d7a2c2c7784365000783/
  */
 
-const balancedParens = n => {
+const balancedParens = (n: number) => {
   switch (n) {
     case 0:
       return [''];
@@ -19,7 +19,26 @@ const balancedParens = n => {
     default:
       break;
   }
-  return [];
+  const result: Set<string> = new Set();
+  const previousResult = balancedParens(n - 1);
+  previousResult.map(parensSet => {
+    result.add('()' + parensSet);
+    result.add(parensSet + '()');
+    result.add(`(${parensSet})`)
+    let index = 0;
+    while (index < parensSet.length) {
+      const nextClosedParenIndex = parensSet.indexOf('()', index);
+      if (nextClosedParenIndex > 0) {
+        result.add(parensSet.slice(0, nextClosedParenIndex + 1) + '()' + parensSet.slice(nextClosedParenIndex + 1));
+        result.add(parensSet.slice(0, nextClosedParenIndex) + '()' + parensSet.slice(nextClosedParenIndex));
+        result.add(parensSet.slice(0, nextClosedParenIndex + 2) + '()' + parensSet.slice(nextClosedParenIndex + 2));
+        index = nextClosedParenIndex + 1;
+      } else index = parensSet.length;
+    }
+    return null;
+  });
+
+  return Array.from(result);
 }
 
 export { balancedParens };
